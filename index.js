d8525d085fbf935a1090ee2048668f9f497c4b6d
abir301 <abir.hasan2003s@gmail.com>
2025-10-06 21:27:51 +0600

    update operation added

--- index.js
+++ index.js
@@ -36,14 +36,33 @@ async function run() {
 
     app.get('/all-books', async (req, res) => {
       const cursor = addBook.find();
-      const result = await cursor.toArray();
+      const result = await cursor.toArray(); 
+      res.send(result);
+    })
+
+      app.get('/all-books/:id', async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) }
+      const result = await addBook.findOne(query);
+      res.send(result)
+    })
+
+    app.patch('/all-books/:id' , async(req , res)=>{
+      const id = req.params.id;
+      const filter = {_id: new ObjectId(id)}
+      const options = {upsert: false};
+      const updatedBook = req.body
+      const updateDoc = {
+        $set: {title: updatedBook.title , author: updatedBook.author , genre: updatedBook.genre , isbn: updatedBook.isbn , copies: updatedBook.copies , description: updatedBook.description , bookCover: updatedBook.bookCover , availability: updatedBook.availability }
+      };
+      const result = await addBook.updateOne(filter , updateDoc  , options)
       res.send(result);
     })
 
     // Connect the client to the server	(optional starting in v4.7)
     //await client.connect();
     // Send a ping to confirm a successful connection
-   // await client.db("admin").command({ ping: 1 });
+    // await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
   } finally {
     // Ensures that the client will close when you finish/error
@@ -54,7 +73,7 @@ async function run() {
 run().catch(console.dir);
 
 
-app.get('/', (req, res) => {
+app.get('/', (req, res) => { 
   res.send('Server is running')
 })
 
